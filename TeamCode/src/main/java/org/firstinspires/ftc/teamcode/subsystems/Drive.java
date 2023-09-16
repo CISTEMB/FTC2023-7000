f9fe33a29cb57c9d22d9784568c093e170f86614
Ryan Sjostrand <ryansjostrand@gmail.com>
2023-09-16 12:02:56 -0500

    made drive subsystem
    made by: me, Christian

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Drive extends SubsystemBase {
+    private DcMotor leftFrontMotor;
+    private DcMotor leftBackMotor;
+    private DcMotor rightFrontMotor;
+    private DcMotor rightBackMotor;
+    private Telemetry t;
+
+    public Drive(HardwareMap hardwareMap, Telemetry t) {
+        this.t = t;
+
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
+
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    public void arcadeDrive(double forward, double turn, double strafe, boolean curve) {
+        if (curve) {
+            forward = inputCurve(forward);
+            turn = inputCurve(turn);
+            strafe = inputCurve(strafe);
+        }
+
+        //Calculate speed for each motor
+        double frontLeft = forward + turn + strafe;
+        double frontRight = forward - turn - strafe;
+        double backLeft = forward + turn - strafe;
+        double backRight = forward - turn + strafe;
+
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //set motor
+        leftFrontMotor.setPower(frontLeft);
+        leftBackMotor.setPower(backLeft);
+        rightFrontMotor.setPower(frontRight);
+        rightBackMotor.setPower(backRight);
+    }
+
+    public void stop() {
+        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public double inputCurve(double input) {
+        if (0 <= input) {
+            // input is positive
+            return input * input;
+        } else {
+            // input is negative
+            return -(input * input);
+        }
+
+    }
+}
